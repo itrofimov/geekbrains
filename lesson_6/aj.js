ca604346f43535f4698ae4f9b282bb7323592c45
Ilia Trofimov <ilia.trofimov@gmail.com>
2016-02-09 14:45:37 +0300

    initial lesson 6 HW commit

---
+++ lesson_6/aj.js
@@ -0,0 +1,20 @@
+(function($){  //обертка для jQuery
+	$(function(){// onload
+		//вешаем событие
+		$('#validate').on('click',function(){
+			$('form span').empty();
+			$('#response').text('Валидация...ждите...').css('color','black');;
+			$.post('validator.php', $('form').children(), function(data){
+				response=JSON.parse(data);
+				if(response.result) $('#response').text('Валидация пройдена').css('color','green');
+				else {
+					$.each(response.error, function(name,value){
+						$('form > *[name="' + name.toLowerCase() + '"]').next('span').text(value);
+						$('#response').text('Валидация не пройдена').css('color','red');
+					});
+				}
+			})
+		});
+		$('form > input, form select').after('<span/>').next('span').css('color','red');
+	});
+})(jQuery);
